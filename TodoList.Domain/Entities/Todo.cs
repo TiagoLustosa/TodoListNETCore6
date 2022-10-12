8d7a409650cdad72c7673d5b83c669b13e6af0b3
TiagoLustosa <tiagolustosasantos@hotmail.com>
2022-10-12 16:26:41 -0300

    Add domain layer and Todo entity

---
+++ TodoList.Domain/Entities/Todo.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoList.Domain.Entities
+{
+    public class Todo
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
