bd750c2714452da8a7aa6e5736cf7c09d786da13
TiagoLustosa <tiagolustosasantos@hotmail.com>
2022-10-12 17:29:39 -0300

    Add migration and create database

--- TodoList.Data/Context/TodoListContext.cs
+++ TodoList.Data/Context/TodoListContext.cs
@@ -1,14 +1,24 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
 using TodoList.Domain.Entities;
 
 namespace TodoList.Infra.Context
 {
     public class TodoListContext : DbContext
     {
-        public TodoListContext(DbContextOptions<TodoListContext> options)
+        protected readonly IConfiguration Configuration;
+
+        public TodoListContext(DbContextOptions<TodoListContext> options, IConfiguration configuration)
                                                           : base(options)
         {
+            Configuration = configuration;
         }
         public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"));
+        }
     }
 }
---
+++ TodoList.Data/Migrations/20221012201629_Initial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoList.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Todos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsComplete = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Todos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Todos");
+        }
+    }
+}
