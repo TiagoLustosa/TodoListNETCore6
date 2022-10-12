3d581aa9214c1da20c969795f3b8ef68a2f17f48
TiagoLustosa <tiagolustosasantos@hotmail.com>
2022-10-12 16:29:37 -0300

    Add TodoList Repository Interface

---
+++ TodoList.Domain/Interfaces/ITodoListRepository.cs
@@ -0,0 +1,13 @@
+﻿using TodoList.Domain.Entities;
+
+namespace TodoList.Domain.Interfaces
+{
+    public interface ITodoListRepository
+    {
+        Task<Todo> GetTodoById(Guid id);
+        Task<IEnumerable<Todo>> GetAllTodos();
+        Task<Todo> AddTodo(Todo todo);
+        Task UpdateTodo(Todo todo);
+        Task RemoveTodo(Todo todo);
+    }
+}
