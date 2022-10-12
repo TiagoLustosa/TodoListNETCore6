b54c8eb33691065c5666956ffd5106f5ae7accc2
TiagoLustosa <tiagolustosasantos@hotmail.com>
2022-10-12 17:36:13 -0300

    Add TodoListService interface

---
+++ TodoList.Domain/Interfaces/ITodoListService.cs
@@ -0,0 +1,11 @@
+﻿using TodoList.Domain.Entities;
+
+namespace TodoList.Domain.Interfaces
+{
+    public interface ITodoListService
+    {
+        Task RemoveTodo(Guid id);
+
+        Task<Todo> UpdateTodo(Guid id, Todo todo);
+    }
+}
