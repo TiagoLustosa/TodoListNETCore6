8dee654e146a84067a0985461c55f78e8562d7f1
TiagoLustosa <tiagolustosasantos@hotmail.com>
2022-10-12 16:34:52 -0300

    Add Todolist Db context

---
+++ TodoList.Data/Context/TodoListContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoList.Domain.Entities;
+
+namespace TodoList.Infra.Context
+{
+    public class TodoListContext : DbContext
+    {
+        public TodoListContext(DbContextOptions<TodoListContext> options)
+                                                          : base(options)
+        {
+        }
+        public DbSet<Todo> Todos { get; set; }
+    }
+}
