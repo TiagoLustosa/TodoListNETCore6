09e721eaad9e86c7fb16fb7a918f990c5263e7f5
TiagoLustosa <tiagolustosasantos@hotmail.com>
2022-10-12 17:47:01 -0300

    Add TodoList Service

---
+++ TodoList.Application/Services/TodoListService.cs
@@ -0,0 +1,31 @@
+﻿using TodoList.Domain.Entities;
+using TodoList.Domain.Interfaces;
+
+namespace TodoList.Application.Services
+{
+    public class TodoListService : ITodoListService
+    {
+        private readonly ITodoListRepository _todoListRepository;
+        public TodoListService(ITodoListRepository todoListRepository)
+        {
+            _todoListRepository = todoListRepository;
+        }
+        public async Task RemoveTodo(Guid id)
+        {
+            var todo = await _todoListRepository.GetTodoById(id);
+            await _todoListRepository.RemoveTodo(todo);
+        }
+
+        public async Task<Todo> UpdateTodo(Guid id, Todo todo)
+        {
+            var todoFromRepository = await _todoListRepository.GetTodoById(id);     
+            todoFromRepository.Title = todo.Title;
+            todoFromRepository.Description = todo.Description;
+            todoFromRepository.EndDate = todo.EndDate;
+
+            await _todoListRepository.UpdateTodo(todoFromRepository);
+
+            return todoFromRepository;
+        }
+    }
+}
